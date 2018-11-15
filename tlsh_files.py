f2017cf8611c15df4e1ea2c2b9c25f803db9a15e
Sarah Helble <shelble2@jhu.edu>
2018-11-15 03:14:54 -0500

    divide number of differences found in TLSH by the number of bytes in the hash to get a percentage comparable to other algs

--- tlsh_files.py
+++ tlsh_files.py
@@ -3,6 +3,11 @@ import tlsh
 import argparse
 import ssdeep
 
+HASH_LEN = 35 # Number of bytes in the hashes produced by TLSH. Differences
+              # are counted by TLSH in accordance with the number of bytes
+              # that differ between hashes. This is used to obtain a
+              # percentage like other metrics.
+
 def tlsh_files(file1, file2):
     with open(file1, 'rb') as f1:
         hash1 = tlsh.hash(f1.read())
@@ -15,7 +20,10 @@ def tlsh_files(file1, file2):
     differences = tlsh.diff(hash1, hash2)
     print("Differences: %d" % differences)
 
-    return hash1, hash2, differences
+    similarity = (( HASH_LEN - float(differences) ) / HASH_LEN )* 100
+    print("similarity: %d" % similarity)
+    
+    return hash1, hash2, int(similarity)
 
 def main():
     parser = argparse.ArgumentParser(description="perform tlsh hash on two files and compare")
